4ba28417b79adc8bc37199e204d8d5cb3e9a9878
ahmed Elsherif <162807418+ahmed2005hussen@users.noreply.github.com>
2025-05-05 21:36:32 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,181 @@
+#include <iostream>
+
+using namespace std;
+
+class heap {
+private:
+    int *arr;
+    int size;
+    int heapSize;
+public:
+    heap(int s = 10) {
+        size = s;
+        heapSize = 0;
+        arr = new int[s];
+    }
+
+    heap(int arr[], int n) {
+
+        size = n+1;
+        heapSize = n;
+        this->arr = new int[size];
+        for (int i = 1; i < size; i++) {
+            this->arr[i] = arr[i];
+        }
+    }
+
+    void MAX_HEAPIFY(int i){
+        int largest = i;
+        int l = i*2; int r = l + 1;
+        if(l <= heapSize && arr[l] > arr[largest]){
+            largest = l ;
+        }
+
+        if(r<= heapSize && arr[r] > arr[largest]){
+            largest = r ;
+        }
+
+        if(largest != i ){
+             swap(arr[i], arr[largest]);
+            MAX_HEAPIFY(largest);
+        }
+    }
+
+    void BUILD_MAX_HEAP() {
+        for(int i = heapSize/2 ; i >= 1; i--){
+            MAX_HEAPIFY(i);
+        }
+    }
+
+    void HEAPSORT() {
+        int newsize = heapSize;
+
+        for(int i = heapSize ; i >=2; i--){
+            swap(arr[i],arr[1]);
+            heapSize--;
+            MAX_HEAPIFY(1);
+        }
+        heapSize = newsize;
+    }
+
+    void print(){
+        if(heapSize==0){
+            cout<< "empty heap "<< endl;
+            return;
+        }
+        cout<< "Heap is: ";
+        for(int i = 1 ; i <= heapSize ;i++){
+            cout<< arr[i] << " ";
+        }
+        cout<< endl;
+    }
+
+
+    ~heap() {
+        delete[]arr;
+    }
+};
+
+class Priority_Queues{
+private:
+    int *arr;
+    int size;
+    int heapSize;
+public:
+
+    Priority_Queues(int n = 10){
+        size = n;
+        heapSize = 0;
+        arr = new int[n+1] ;
+    }
+
+
+    void MAX_HEAPIFY(int i){
+        int largest = i;
+        int l = i*2; int r = l + 1;
+        if(l <= heapSize && arr[l] > arr[largest]){
+            largest = l ;
+        }
+
+        if(r<= heapSize && arr[r] > arr[largest]){
+            largest = r ;
+        }
+
+        if(largest != i ){
+            swap(arr[i], arr[largest]);
+            MAX_HEAPIFY(largest);
+        }
+    }
+
+    void INSERT(int n){
+        if(heapSize + 1 == size){
+            int *temp = new int[size*2];
+            for(int i = 1; i<= size; i++){
+                temp[i]=arr[i];
+            }
+            delete []arr;
+            arr = temp;
+            size = size*2;
+        }
+        arr[++heapSize] = INT_MIN;
+        INCREASE_KEY(heapSize,n);
+    }
+
+   int EXTRACT_MAX(){
+        if(heapSize==0){
+        throw runtime_error("empty");
+        }
+        int max = arr[1];
+        swap(arr[1],arr[heapSize]);
+        heapSize--;
+        MAX_HEAPIFY(1);
+       return max;
+    }
+
+    int  MAXIMUM(){
+        if(heapSize==0){
+            throw runtime_error("empty");
+        }
+        return arr[1];
+    }
+
+    void INCREASE_KEY(int x,int k){
+         if(k<arr[x])
+         {
+             cout<< "this is small than the current key" <<endl;
+         }
+         arr[x]=k;
+         while(x>1){
+             if(arr[x] > arr[x/2]){
+                 swap(arr[x] , arr[x/2]);
+             }
+             x = x / 2;
+         }
+    }
+
+
+    ~Priority_Queues(){
+        delete []arr;
+    }
+};
+
+int main(){
+  Priority_Queues a;
+  a.INSERT(2);
+  a.INSERT(122);
+  a.INSERT(23);
+  a.INSERT(21);
+  a.INSERT(1112);
+    cout<< a.MAXIMUM() <<endl;
+   a.EXTRACT_MAX();
+    cout<< a.MAXIMUM() <<endl;
+    a.EXTRACT_MAX();
+    cout<< a.MAXIMUM() <<endl;
+    a.EXTRACT_MAX();
+    cout<< a.MAXIMUM() <<endl;
+    a.EXTRACT_MAX();
+    cout<< a.MAXIMUM() <<endl;
+    a.EXTRACT_MAX();
+    cout<< a.MAXIMUM() <<endl;
+
+}
